af7831cf235a2df60db083dda6bc7730d18631b6
Arteev Raina <arteevraina@gmail.com>
2020-07-08 21:55:58 +0530

    feat: adding quizzler app :bulb:

---
+++ quizzler-flutter/lib/questions.dart
@@ -0,0 +1,10 @@
+class Question {
+  String questionText;
+  bool questionAnswer;
+
+  //This is a constructor
+  Question({String q, bool a}) {
+    questionText = q;
+    questionAnswer = a;
+  }
+}
